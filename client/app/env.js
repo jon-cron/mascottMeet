21e7a41eb28b56742674599d8e7f9d5b3dbeb477
jon-cron <jonathanacm95@gmail.com>
2022-12-01 16:16:31 -0700

    init

---
+++ client/app/env.js
@@ -0,0 +1,7 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-esg3qw0wv4oc3r11.us.auth0.com'
+export const audience = 'https://classroom.com'
+export const clientId = 'hulGXtuEYnFl2eBkOovdhEdr7rwSZ9J1'
+
