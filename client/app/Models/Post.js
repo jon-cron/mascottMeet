8d3b7d97c250d5d1486f0f695e4a985484d25c99
jon-cron <jonathanacm95@gmail.com>
2022-12-02 11:15:21 -0700

    fixed template

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -1,10 +1,12 @@
 export class Post{
   constructor(data){
-    this.name = data.name,
+    this.name = data.name
     this. description = data.description
     this.img = data.img
     this.postId = data.postId
     this.likes= data.likes
+    this.name = data.postedBy.name
+    this.picture = data.postedBy.picture
 
   }
 
@@ -15,8 +17,16 @@ export class Post{
       <img class="img-fluid" src="${this.img}" alt="">
     </div>
     <div class="col-6">
+    <div>
       <h1>${this.name} like ${this.likes}</h1>
-      <p>${this.description}</p>
+      </div>
+      <div class="mt-5">
+      <h4>Posted By:</h4>
+      <div class="d-flex align-items-center">
+      <img class="poster-img m-1" src="${this.picture}" alt="">
+      <h6>${this.name}</h6>
+      </div>
+      </div>
     </div>
     <div class="col-3 d-flex justify-content-center align-items-center">
       <button class="btn text-center" onclick="app.likersController.likePost('${this.postId})"><h1><i class="mdi mdi-heart"></i></h1></button>
