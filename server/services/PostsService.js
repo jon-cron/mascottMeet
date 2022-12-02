c69dc4a5a16dc6171ae975cd1ad687958b2e20cd
moathabdulrazak <moathabdulrazak12@gmail.com>
2022-12-02 11:52:59 -0700

    like count

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -6,18 +6,19 @@ import { BadRequest } from "../utils/Errors.js";
 
 class PostsService {
   async getPosts() {
-    const posts = await dbContext.Posts.find().populate('postedBy')
+    const posts = await dbContext.Posts.find().populate('postedBy likeCount')
     return posts
   }
 
   async getPostById(id) {
-    const post = await dbContext.Posts.findById(id).populate('postedBy')
+    const post = await dbContext.Posts.findById(id).populate('postedBy likeCount')
     if (!post) { throw new BadRequest('can not locate post') }
     return post
   }
 
   async createPost(postData) {
     const newPost = await dbContext.Posts.create(postData)
+    newPost.populate('postedBy likeCount')
     return newPost
 
   }
