2d19251497e9620463b15884cb949fd39a6575bb
jon-cron <jonathanacm95@gmail.com>
2022-12-02 16:57:32 -0700

    comments populate

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,5 +1,6 @@
 import { dbContext } from "../db/DbContext.js";
 import { BadRequest } from "../utils/Errors.js";
+import { logger } from "../utils/Logger.js";
 
 
 
@@ -7,23 +8,24 @@ import { BadRequest } from "../utils/Errors.js";
 class PostsService {
   async getPosts() {
     const posts = await dbContext.Posts.find().populate('postedBy likeCount comments')
+    logger.log(posts)
     return posts
   }
 
   async getPostById(id) {
-    const post = await dbContext.Posts.findById(id).populate('postedBy likeCount comments')
+    const post = await dbContext.Posts.findById(id).populate('postedBy likeCount')
     if (!post) { throw new BadRequest('can not locate post') }
     return post
   }
 
   async createPost(postData) {
     const newPost = await dbContext.Posts.create(postData)
-    await newPost.populate('postedBy likeCount comments')
+    await newPost.populate('postedBy likeCount')
     return newPost
   }
-  async remove(id) {
+ async remove(id){
     const post = await dbContext.Posts.findById(id)
-    if (!post) throw new BadRequest('no post by this id')
+    if(!post) throw new BadRequest('no post by this id')
     await post.remove()
     return `deleted ${post.name}`
   }
